44eb5c572e69192827f89eb4050ed20371d404ba
salimaelayy <64556840+salimaelayy@users.noreply.github.com>
2024-03-21 09:55:06 +0000

    ggg

--- Models/Note.js
+++ Models/Note.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 const noteSchema = new mongoose.Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
+  date: { type: Date, default: Date.now },
 });
 
 const Note = mongoose.model('Note', noteSchema);
